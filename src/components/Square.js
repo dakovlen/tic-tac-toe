9bd74eead090b01c3faef4b03ac013764be8e1d5
Artem Koval <dakovlen@gmail.com>
2023-01-29 14:44:59 +0200

    Final version class components app

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,13 +1,12 @@
 import React from "react";
 
 function Square(props) {
-    return (
-      <button
-        className="square"
-        onClick={props.onClick}
-      >
-        {props.value}
-      </button>
-    );
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 export default Square
